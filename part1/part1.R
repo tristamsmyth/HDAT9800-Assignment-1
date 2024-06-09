b77c611fef45e2f263c559b8d6933932f0d97b0f
tristamsmyth <tms.cardiac@gmail.com>
2024-06-09 11:51:51 +1000

    HIV3 changes

--- part1/part1.R
+++ part1/part1.R
@@ -1,4 +1,8 @@
 # assignment 1 term 2 2024 9800
+# to import dataset click on data set in folder "part 1"
+library(dplyr)
+library(dplyrAssist)
+
 HIV <- ZZZ_C001_FakeHIV
 
 dim(HIV)
@@ -6,11 +10,14 @@ head(HIV, 10)
 
 colnames(HIV)
 
+dim(HIV)
+head(HIV, 10)
+
+
 # filtering is used on ROWS - here we have first timepoint
-filter (HIV, Timepoints == 0 )
+filter(HIV, Timepoints==0)
 
-
-select(HIV, "Base Drug Combo" == 1)
+select(HIV, Base Drug Combo == 3)
 
 # here we are arranging patients in descedning CD4 counts
 arrange(HIV, desc(CD4))
